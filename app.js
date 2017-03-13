bc876faf0a56fd4f7551b1cada5ccd3a85377193
denysbsb <denys.design@gmail.com>
2017-03-13 16:34:58 -0300

    error handler

--- app.js
+++ app.js
@@ -19,8 +19,21 @@ app.get('/',function(req, res){
   res.json({'name':'Romulo', 'email': 'wromulo@gmail.com'});
 });
 
+//error handling
+app.use(function(request, response, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, request, response, next) {
+  response.status(err.status || 500).json({ err: err.message });
+});
+
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http:%s:%s',host,port);
 });
+
+module.exports = app;
