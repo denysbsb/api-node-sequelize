e682759563086061c9dbff041fcb9da8c3a76631
denysbsb <denys.design@gmail.com>
2017-03-13 16:30:09 -0300

    base app.js

--- app.js
+++ app.js
@@ -1,90 +1,26 @@
 var express = require('express');
 var app = express();
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var Sequelize = require('sequelize');
+app.use(methodOverride('X-HTTP-Method'));
+app.use(methodOverride('X-HTTP-Method-Override '));
+app.use(methodOverride('X-Method-Override'));
+app.use(methodOverride('_method'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(cors());
+
+app.get('/',function(req, res){
+  res.status(201);
+  res.json({'name':'Romulo', 'email': 'wromulo@gmail.com'});
 });
 
-var user = 'postgres';
-var pw = 'a1920212223';
-var host = 'localhost';
-var database = 'siconv';
-
- var sequelize = new Sequelize(database, user, pw, {
-        host: host,
-        port: "5432", 
-        dialect: 'postgres',
-        pool: {
-            max: 50,
-            min: 0,
-            idle: 10000
-        }
- });
-
- var User = sequelize.define('programas', {
-  nome: {
-    type: Sequelize.STRING,
-    field: 'nome' // Will result in an attribute that is firstName when user facing but first_name in the database
-  },
-  tipo: {
-    type: Sequelize.STRING
-  },
-  local: {
-    type: Sequelize.STRING
-  }
-}, {
-  freezeTableName: false // Model tableName will be the same as the model name
-});
-
-function horaDoShow(){
-
-
-
-sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
-
-  
-
-User.sync({force: false}).then(function () {
-  // Table created
-  return User.create({
-    nome: 'Eike',
-    tipo: 'Hancock',
-    local: 'CE'
-  });
-});
-
-}
-
-horaDoShow();
-
-app.get('/api/programas',function(req, res){
-    User.findAll().then(function (resp) {
-      res.status(200).json(resp);
-    });
-});
-
-app.get('/api/programas/:userId',function(req, res){
-  
-  var userId = req.params.userId;
-
-    User.findAll({
-        where: {
-            id: userId
-        }
-    }).then(function (data) {
-        res.status(200).json(data);
-    });
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http:%s:%s',host,port);
 });
