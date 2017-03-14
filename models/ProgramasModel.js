9149f101eb2da6417cfa53f3d083fb6afa503452
denysbsb <denys.design@gmail.com>
2017-03-14 14:33:51 -0300

    model programas

---
+++ models/ProgramasModel.js
@@ -0,0 +1,15 @@
+function ProgramasModel() {}
+
+ProgramasModel.prototype.find = function(query, callback) {
+};
+
+ProgramasModel.prototype.create = function(data, callback) {
+};
+
+ProgramasModel.prototype.update = function(_id, data, callback) {
+};
+
+ProgramasModel.prototype.remove = function(_id, callback) {
+};
+
+module.exports = new ProgramasModel();
